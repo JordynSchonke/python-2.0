195664ec7f7f85fc7c723b194ef7fc122e41247e
JordynSchonke <rachel.schonke@gmail.com>
2026-04-24 17:51:06 +0200

    Add files via upload

---
+++ revision/main.py
@@ -0,0 +1,16 @@
+#print statement
+print('my name is jordyn')
+print('hello')
+print('hello'+' my name is jordyn')
+print('hello '+ str(3))
+print('my name is jordyn '+'\nand i am '+ str(13)+' years old')
+
+#variables
+name='jordyn'
+print(name)
+
+colour='purple'
+print('my favoriete colour is '+ colour)
+
+height=1.60
+print('my height is ' + str(height))
