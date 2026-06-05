cf3199109f9e0b9c55fd689354be0dd84226ef3c
JordynSchonke <rachel.schonke@gmail.com>
2026-06-05 17:52:17 +0200

    Add files via upload

---
+++ codisinal programming/main.py
@@ -0,0 +1,44 @@
+i=7
+if i <5:
+    print('i value is less')
+else:
+    print('i value is greater')
+
+weather='sunny'
+if weather=='wind':
+    print('it is too hot too day')
+else:
+    print('it is not that hot too day')
+
+name=input('what is your name ?')
+print(name)
+
+number=int(input('give me a number'))
+print(number)
+if number >0:
+    print('it is a positieve number')
+else:
+    print('it is a negatieve number')
+
+temperature=int(input('what is the temperature ?'))
+print(temperature)
+if temperature >25:
+    print('it is time for ice cream!')
+else:
+    print('lets have a hot chocolate!')
+
+animal=input('enter an animal dog,cat or cow')
+if animal=='dog':
+    print('woef')
+elif animal=='cat':
+    print('miauw')
+else:
+    print('moo')
+
+score=int(input('what is your score ?'))
+if score==100:
+    print('golden trophy')
+elif score >50:
+    print('silver trophy')
+else:
+    print('keep practicing')
